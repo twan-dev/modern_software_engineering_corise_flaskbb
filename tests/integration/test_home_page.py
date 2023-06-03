23b6863aba5ef8329c7acad8ea3aa5208db61de5
Max Allen <max.greathouse.allen@gmail.com>
2023-06-03 13:32:56 -0700

    Boilerplate part 4

---
+++ tests/integration/test_home_page.py
@@ -0,0 +1,10 @@
+###################################################################
+# CoRise TODO: add an integration test that uses the test client to
+# load the home page ('/').
+#
+# Hint: you can get the test client by calling `application.test_client()`
+# when using the application test fixture.
+
+# ADD CODE HERE
+
+###################################################################
