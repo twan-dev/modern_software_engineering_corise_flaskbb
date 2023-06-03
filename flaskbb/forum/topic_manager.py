01aa49052556f04a1a1f8dd9bbca6d74bd1db291
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2023-06-03 17:05:11 +0000

    Finished up topic manager and tests

--- flaskbb/forum/topic_manager.py
+++ flaskbb/forum/topic_manager.py
@@ -1,7 +1,10 @@
-# CoRise TODO: implement a class for managing topics with in a forum
+"""
+A class for managing topic state.
+
+It operates on a list of topics and makes sure that the desired state is the actual state of the topic
+"""
 class TopicManager(object):
-    def __init__(self, user, topics) -> None:
-        self.__user = user
+    def __init__(self, topics) -> None:
         self.__topics = topics
 
     def set_lock_state(self, desired_state):
@@ -9,6 +12,27 @@ class TopicManager(object):
 
     def set_important_state(self, desired_state):    
         return self.__set_field_state('important', desired_state)
+    
+    def delete_topics(self):
+        modified_topics = 0
+        for topic in self.__topics:
+            topic.delete()
+            modified_topics+=1
+        
+        return modified_topics
+    
+    def set_hidden_state(self, user, desired_state):
+        modified_topics = 0
+        for topic in self.__topics:
+            if topic.hidden != desired_state:
+                if topic.hidden:
+                    topic.unhide()
+                else:
+                    topic.hide(user)
+                topic.save()   
+                modified_topics+=1
+        
+        return modified_topics
     
     # Sets the specified field to the desired state
     def __set_field_state(self, field, desired_state):
--- tests/unit/forum/test_topic_manger.py
+++ tests/unit/forum/test_topic_manger.py
@@ -1,40 +1,62 @@
 import pytest
 
 from flaskbb.forum.topic_manager import TopicManager
-# CoRise TODO: add unit tests for topic manager
+from flaskbb.forum.models import Topic
 
 class TestTopicManager(object):
 
-    def test_set_lock_state(user, topic):
-        subject = TopicManager(user, [topic])
+    def test_set_lock_state(self,topic):
+        subject = TopicManager([topic])
         result = subject.set_lock_state(True)
 
         assert result == 1
         assert topic.locked == True
 
-    def test_set_lock_state_doesnot_modify_topic_with_desired_state(user, topic_locked):
-        subject = TopicManager(user, [topic_locked])
+    def test_set_lock_state_doesnot_modify_topic_with_desired_state(self, topic_locked):
+        subject = TopicManager([topic_locked])
         result = subject.set_lock_state(True)
 
         assert result == 0
 
-    def test_set_lock_state_handles_each_topic(user, topic, topic_locked):
-        subject = TopicManager(user, [topic_locked, topic])
+    def test_set_lock_state_handles_each_topic(self, topic, topic_locked):
+        subject = TopicManager([topic_locked, topic])
         result = subject.set_lock_state(False)
 
         assert result == 1
 
-    def test_set_imporant_state(user, topic):
-        subject = TopicManager(user, [topic])
+    def test_set_imporant_state(self, topic):
+        subject = TopicManager([topic])
         result = subject.set_important_state(True)
 
         assert result == 1
         assert topic.important == True
 
-    def test_set_imporant_state_handles_each_topic(user, topic, topic_important):
-        subject = TopicManager(user, [topic_important, topic])
+    def test_set_imporant_state_handles_each_topic(self, topic, topic_important):
+        subject = TopicManager([topic_important, topic])
         result = subject.set_important_state(False)
 
         assert result == 1
         assert topic_important.important == False
-        
+
+    def test_delete_topics(self, topic, topic_important):
+        subject = TopicManager([topic_important, topic])
+        result = subject.delete_topics()
+
+        assert result == 2
+        assert Topic.query.filter_by(id=topic.id).first() is None
+        assert Topic.query.filter_by(id=topic_important.id).first() is None
+
+    def test_set_hidden_state_to_false(self, user, topic, topic_important, topic_hidden):
+        subject = TopicManager([topic_important, topic, topic_hidden])
+        result = subject.set_hidden_state(user, False)
+
+        assert result == 1
+        assert topic_hidden.hidden == False
+
+    def test_set_hidden_state_to_true(self, user, topic, topic_important, topic_hidden):
+        subject = TopicManager([topic_important, topic, topic_hidden])
+        result = subject.set_hidden_state(user, True)
+
+        assert result == 2
+        assert topic.hidden == True
+        assert topic_important.hidden == True
