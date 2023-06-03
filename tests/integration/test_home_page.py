96d090e125387dc7a3ebc7ebd36f2e4417be6807
Max Allen <max.greathouse.allen@gmail.com>
2023-06-03 13:34:55 -0700

    Boilerplate part 4 nit

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -1,6 +1,8 @@
 ###################################################################
 # CoRise TODO: add an integration test that uses the test client to
-# load the home page ('/').
+# load the home page ('/'). Make sure the response code is 200 and
+# that the response data contains something you expect to see on the
+# home page.
 #
 # Hint: you can get the test client by calling `application.test_client()`
 # when using the application test fixture.
