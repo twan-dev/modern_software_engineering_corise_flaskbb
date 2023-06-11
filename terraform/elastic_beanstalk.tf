095ba4232c7aaaf7e91fce1f1346c2c1907379b1
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2023-06-11 23:21:26 +0000

    Fixing environment version

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -5,7 +5,7 @@ resource "aws_elastic_beanstalk_application" "application" {
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "flaskbb-environment"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.0 running Python 3.11"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Python 3.11"
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
